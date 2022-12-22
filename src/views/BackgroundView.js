c02f49dbcfd4ac1928d26f1e1d2178a8007f9a54
benronkth <benron@kth.se>
2022-12-22 23:29:15 +0100

    Adds backgorund. Resolves #1

---
+++ src/views/BackgroundView.js
@@ -0,0 +1,11 @@
+function BackgroundView() {
+    return (<div>
+
+        <div className="background">
+            <h1>Hej Bashirrrrr</h1>
+        </div>
+
+    </div>);
+}
+
+export default BackgroundView;
