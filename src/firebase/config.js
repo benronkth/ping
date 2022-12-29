797728f18701e7040546a5cec7952f94294c7ba2
benronkth <benron@kth.se>
2022-12-29 15:21:13 +0100

    Adds border to tank

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDeBAuU2kGhlmVIM0AbjcwFInX46Iy7odg",
+    authDomain: "tanksss.firebaseapp.com",
+    projectId: "tanksss",
+    storageBucket: "tanksss.appspot.com",
+    messagingSenderId: "133968125770",
+    appId: "1:133968125770:web:8896c459152dc4b8b3abb3",
+    measurementId: "G-HRTRYNH2SV",
+    databaseURL: "https://tanksss-default-rtdb.europe-west1.firebasedatabase.app",
+};
